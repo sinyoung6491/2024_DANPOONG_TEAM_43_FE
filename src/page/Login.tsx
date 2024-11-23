a54dfb1852e1c49ef09db1680e69f9b0648fdbc9
sin._.0_ <138271153+sinyoung6491@users.noreply.github.com>
2024-11-24 07:12:01 +0900

    Update Login.tsx

--- src/page/Login.tsx
+++ src/page/Login.tsx
@@ -32,7 +32,7 @@ const Login = () => {
 
   const handleKakaoLogin = () => {
     const clientId = '78256822bbbbbe614142bcad43930708';
-    const redirectUri = 'https://carely-seven.vercel.app/kakao-login';
+    const redirectUri = 'http://localhost:3000/kakao-login';
     window.location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=profile_nickname`;
   };
 
